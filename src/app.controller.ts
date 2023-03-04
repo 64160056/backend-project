a60a0cedfad42c278554bb5f9c80a4cdf5c6d4ce
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-03-04 23:04:03 +0700

    Auth Module and Auth contants

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport/dist/auth.guard';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req);
   }
 
   @Get()
